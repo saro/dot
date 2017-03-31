4d30aac091393c1771f92ffae1e41dd9490c182e
Rosario Contarino <contarino.rosario@gmail.com>
2017-03-31 12:26:08 +0200

    Post omz-liberation fixes

--- shell/termsupport.zsh
+++ shell/termsupport.zsh
@@ -69,7 +69,7 @@ function omz_termsupport_preexec {
   fi
 
   # cmd name only, or if this is sudo or ssh, the next cmd
-  local CMD=${1[(wr)^(*=*|sudo|ssh|ssk|mosh|rake|-*)]:gs/%/%%}
+  local CMD=${1[(wr)^(*=*|sudo|ssh|s|ssk|mosh|rake|-*)]:gs/%/%%}
   local LINE="${2:gs/%/%%}"
 
   title '$CMD' '%100>...>$LINE%<<'
