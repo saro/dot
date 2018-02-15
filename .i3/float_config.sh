d122aa137cf9c4034c0302e2ccd3129a8a241f80
Rosario Contarino <contarino.rosario@gmail.com>
2018-02-15 18:24:58 +0100

    Replace terminator with xst

--- .i3/float_config.sh
+++ .i3/float_config.sh
@@ -7,13 +7,13 @@ DISPLAY_W=`xrandr|grep current|awk {'print $8'}`
 DISPLAY_H=`xrandr|grep current|awk {'print $10'}|cut -d, -f1`
 
 cat <<< "
-# Set terminator (width = 100%; height 60%)
+# Set floating terminal (width = 100%; height 60%)
 for_window [instance="ttttt"] move to scratchpad
 bindcode 133 [instance="ttttt"] scratchpad show
 for_window [instance="ttttt"] move position 0 30
 for_window [instance="ttttt"] resize set ${DISPLAY_W} $((${DISPLAY_H} * 60 / 100))
 for_window [instance="ttttt"] scratchpad show
-exec terminator -c ttttt -l "float"
+exec st -n 'ttttt' -e sh -c 'TERM=xterm-256color source ~/shell/tmux && tx'
 
 # Add Pulsemixer floating window (width = 100%; height 40%; pos_x = 0%; pos_y = 50%)
 for_window [instance="pulsemixer"] move to scratchpad
@@ -21,7 +21,7 @@ bindsym Mod1+F3 [instance="pulsemixer"] scratchpad show
 for_window [instance="pulsemixer"] move position 0 $((${DISPLAY_H} * 50 / 100))
 for_window [instance="pulsemixer"] resize set ${DISPLAY_W} $((${DISPLAY_H} * 40 / 100))
 for_window [instance="pulsemixer"] scratchpad show
-exec terminator -l pulsemixer -c "pulsemixer"
+exec st -n 'pulsemixer' -e sh -c 'TERM=xterm-256color pulsemixer'
 
 # Set piding (width = 27%; height 70%; pos_x = 73%; pos_y = 20%)
 # for_window [window_role="conversation"] move to scratchpad
@@ -39,5 +39,4 @@ for_window [title="qutebrowser-editor-.*"] move position $((${DISPLAY_W} * 25 / 
 for_window [title="qutebrowser-editor-.*"] resize set $((${DISPLAY_W} * 50 / 100)) $((${DISPLAY_H} * 40 / 100))
 
 " > /tmp/conf_flo
-
 
