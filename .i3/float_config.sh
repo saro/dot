7b3d60ae59777b0568b53841a4fc23d1ec1313fb
Rosario Contarino <contarino.rosario@gmail.com>
2018-11-20 17:50:13 +0100

    Welcome xst

--- .i3/float_config.sh
+++ .i3/float_config.sh
@@ -6,6 +6,8 @@
 DISPLAY_W=`xrandr|grep current|awk {'print $8'}`
 DISPLAY_H=`xrandr|grep current|awk {'print $10'}|cut -d, -f1`
 
+TERMINAL="xst"
+
 cat <<< "
 # Set floating terminal (width = 100%; height 60%)
 for_window [instance="ttttt"] move to scratchpad
@@ -13,15 +15,15 @@ bindcode 133 [instance="ttttt"] scratchpad show
 for_window [instance="ttttt"] move position 0 30
 for_window [instance="ttttt"] resize set ${DISPLAY_W} $((${DISPLAY_H} * 60 / 100))
 for_window [instance="ttttt"] scratchpad show
-exec st -n 'ttttt' -e sh -c 'TERM=xterm-256color source ~/shell/tmux && tx'
+exec ${TERMINAL} -n 'ttttt' -e sh -c 'TERM=xterm-256color source ~/shell/tmux && tx'
 
 # Add Pulsemixer floating window (width = 100%; height 40%; pos_x = 0%; pos_y = 50%)
-for_window [instance="pulsemixer"] move to scratchpad
-bindsym Mod1+F3 [instance="pulsemixer"] scratchpad show
-for_window [instance="pulsemixer"] move position 0 $((${DISPLAY_H} * 50 / 100))
-for_window [instance="pulsemixer"] resize set ${DISPLAY_W} $((${DISPLAY_H} * 40 / 100))
-for_window [instance="pulsemixer"] scratchpad show
-exec st -n 'pulsemixer' -e sh -c 'TERM=xterm-256color pulsemixer'
+# for_window [instance="pulsemixer"] move to scratchpad
+# bindsym Mod1+F3 [instance="pulsemixer"] scratchpad show
+# for_window [instance="pulsemixer"] move position 0 $((${DISPLAY_H} * 50 / 100))
+# for_window [instance="pulsemixer"] resize set ${DISPLAY_W} $((${DISPLAY_H} * 40 / 100))
+# for_window [instance="pulsemixer"] scratchpad show
+# exec ${TERMINAL} -n 'pulsemixer' -e sh -c 'TERM=xterm-256color pulsemixer'
 
 # Set piding (width = 27%; height 70%; pos_x = 73%; pos_y = 20%)
 # for_window [window_role="conversation"] move to scratchpad
