41d856d3c726ed4343cbca4c664ae2beb3e276ca
Rosario Contarino <contarino.rosario@gmail.com>
2019-08-02 14:30:06 +0200

    Add some sanity to the display management mess

---
+++ script/dock_handler.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+set -x
+
+# xrandr --dpi 120
+
+declare -A outputs=( \
+	[VIRTUAL1]="--off"
+	[eDP1]="--mode 1920x1080 --pos 0x0 --rotate normal"
+	[DP1]="--off"
+	[DP2]="--off"
+	[DP2-1]="--off"
+	[DP2-2]="--off"
+	[DP2-3]="--off"
+	[HDMI1]="--off"
+	[HDMI2]="--off"
+)
+
+declare -A configs=( \
+	[docked]="do_docked_conf"
+	[undocked]="do_undocker_conf"
+	[hdmi]="do_external_hdmi"
+)
+
+do_reset_outputs() {
+	outputs[VIRTUAL1]="--off"
+	outputs[eDP1]="--off"
+	outputs[DP1]="--off"
+	outputs[DP2]="--off"
+	outputs[DP2-1]="--off"
+	outputs[DP2-2]="--off"
+	outputs[DP2-3]="--off"
+	outputs[HDMI1]="--off"
+	outputs[HDMI2]="--off"
+}
+
+do_show_usage() {
+	msg="ERROR: valid configurations are: "
+	for cfg in "${!configs[@]}"; do
+		msg="${msg}  [${cfg}]"
+	done
+
+	echo "${msg}"
+}
+
+do_xrand() {
+	local cmd="xrandr "
+
+	for output in "${!outputs[@]}"; do
+		cmd="${cmd} --output ${output} ${outputs[$output]}"
+	done
+
+	xrandr --dpi 120
+	eval "${cmd}"
+}
+
+do_restart_i3() {
+	sleep 1 && ~/.i3/i3_restart.sh
+}
+
+do_docked_conf() {
+	if [[ $(lsusb|grep Lenovo|awk '{print $6}'|head -n1 |cut -f2 -d:) == "1013" ]]; then
+		# Basic dock HOME
+		local res="1920x1080"
+	else
+		# Ultra dock OFFICE
+		local res="2560x1440"
+	fi
+
+	do_reset_outputs
+	outputs[DP2]="--mode ${res} --pos 0x0 --rotate normal"
+}
+
+do_undocker_conf() {
+	do_reset_outputs
+	outputs[eDP1]="--mode 1920x1080 --pos 0x0 --rotate normal"
+}
+
+do_external_hdmi() {
+	echo "NOT READY"
+}
+
+config="${1}"
+
+# Check if requested configuration is valid
+for c in "${!configs[@]}"; do
+	if [[ "${config}" == "${c}" ]]; then
+		is_valid="1"
+		eval "${configs[$c]}"
+	fi
+done
+
+if [[ -n "${is_valid}" ]]; then
+	do_xrand
+	do_restart_i3
+else
+	do_show_usage
+fi
+
