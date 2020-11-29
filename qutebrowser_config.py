5c3d01bb7ed673b3365ce32094295c752d8f4987
Rosario Contarino <contarino.rosario@gmail.com>
2020-11-29 17:39:46 +0100

    Let's stop yt ads in quetebrowser

--- qutebrowser_config.py
+++ qutebrowser_config.py
@@ -1,9 +1,22 @@
+import subprocess
+import os
+from qutebrowser.api import interceptor
+
 config.load_autoconfig()
+
+# ================== Youtube Add Blocking =======================
+def filter_yt(info: interceptor.Request):
+    """Block the given request if necessary."""
+    url = info.request_url
+    if (url.host() == "www.youtube.com" and url.path() == "/get_video_info" and "&adformat=" in url.query()):
+        info.block()
+
+interceptor.register(filter_yt)
 
 c.aliases = {'q': 'quit', 'w': 'session-save', 'wq': 'quit --save'}
 c.session.lazy_restore = False
 c.backend = 'webengine'
-c.qt.force_software_rendering = 'none'
+c.qt.force_software_rendering = 'qt-quick'
 c.auto_save.session = True
 c.content.autoplay = False
 c.content.cache.size = None
@@ -14,6 +27,7 @@ c.content.javascript.enabled = False
 c.content.plugins = False
 c.content.private_browsing = False
 c.content.xss_auditing = True
+c.content.mute = False;
 c.completion.height = '35%'
 c.completion.quick = True
 c.completion.timestamp_format = '%Y-%m-%d'
@@ -31,11 +45,11 @@ c.hints.next_regexes = ['\\bnext\\b', '\\bmore\\b', '\\bnewer\\b', '\\b[>→≫]
 c.input.forward_unbound_keys = 'none'
 c.input.insert_mode.auto_load = True
 c.input.partial_timeout = 0
-c.input.rocker_gestures = False
+# c.input.rocker_gestures = False
 c.keyhint.delay = 500
 c.messages.timeout = 2000
 c.spellcheck.languages = ['en-GB']
-c.statusbar.hide = False
+# c.statusbar.hide = False
 c.statusbar.padding = {'bottom': 1, 'left': 0, 'right': 0, 'top': 1}
 c.statusbar.position = 'bottom'
 c.tabs.background = True
@@ -56,7 +70,7 @@ c.tabs.indicator.padding = {'bottom': 5, 'left': 0, 'right': 4, 'top': 5}
 c.tabs.pinned.shrink = True
 c.url.auto_search = 'dns'
 c.url.default_page = 'about:blank'
-c.url.searchengines = {'sp': 'https://startpage.com/do/asearch?q={}', 'DEFAULT': 'https://duckduckgo.com/?q={}', 'ei': 'http://www.wordreference.com/enit/{}', 'ie': 'http://www.wordreference.com/iten/{}', 'm': 'https://www.google.com/maps?q={}', 'o': 'https://lexico.com/search?utf8=%E2%9C%93&filter=dictionary&query={}'}
+c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'sp': 'https://startpage.com/do/asearch?q={}', 'ddg': 'https://duckduckgo.com/?q={}', 'ei': 'http://www.wordreference.com/enit/{}', 'ie': 'http://www.wordreference.com/iten/{}', 'm': 'https://www.google.com/maps?q={}', 'o': 'https://lexico.com/definition/{}'}
 c.window.title_format = '{private}//qutebrowser - {perc}{current_title}{title_sep}'
 c.zoom.default = '100%'
 c.zoom.levels = ['25%', '33%', '50%', '67%', '75%', '90%', '100%', '110%', '125%', '150%', '175%', '200%', '250%', '300%', '400%', '500%']
@@ -70,18 +84,20 @@ c.colors.tabs.pinned.odd.bg = 'grey'
 c.colors.tabs.pinned.even.bg = 'grey'
 c.colors.tabs.pinned.selected.even.fg = 'white'
 c.colors.tabs.pinned.selected.even.bg = 'black'
-c.fonts.completion.entry = '10pt share tech mono'
-c.fonts.completion.category = '10pt share tech mono'
-c.fonts.debug_console = '10pt share tech mono'
-c.fonts.downloads = '10pt share tech mono'
-c.fonts.hints = 'bold 10pt share tech mono'
-c.fonts.keyhint = '10pt share tech mono'
-c.fonts.messages.error = '10pt share tech mono'
-c.fonts.messages.info = '10pt share tech mono'
-c.fonts.messages.warning = '10pt share tech mono'
-c.fonts.prompts = '10pt share tech mono'
-c.fonts.statusbar = '10pt share tech mono'
-c.fonts.tabs = '12pt share tech mono'
+c.fonts.completion.entry = '11pt share tech mono'
+c.fonts.completion.category = '11pt share tech mono'
+c.fonts.debug_console = '11pt share tech mono'
+c.fonts.downloads = '11pt share tech mono'
+c.fonts.hints = 'bold 11pt share tech mono'
+c.fonts.keyhint = '11pt share tech mono'
+c.fonts.messages.error = '11pt share tech mono'
+c.fonts.messages.info = '11pt share tech mono'
+c.fonts.messages.warning = '11pt share tech mono'
+c.fonts.prompts = '11pt share tech mono'
+c.fonts.statusbar = '11pt share tech mono'
+c.fonts.tabs.selected = '11pt share tech mono'
+c.fonts.tabs.unselected = '11pt share tech mono'
+# c.fonts.tabs = '12pt share tech mono'
 c.fonts.web.size.minimum_logical = 8
 config.bind(';', 'set-cmd-text :')
 config.bind('<Ctrl+h>', 'tab-prev')
@@ -91,8 +107,8 @@ config.bind('<Ctrl+l>', 'tab-next')
 # config.bind('<Ctrl+v>', None)
 config.bind('<Shift+Space>', 'run-with-count 25 scroll up')
 config.bind('<Space>', 'run-with-count 25 scroll down')
-config.bind('B', 'set-cmd-text -s :buffer')
-config.bind('C', 'inspector')
+config.bind('b', 'set-cmd-text -s :buffer')
+config.bind('C', 'devtools')
 # config.bind('F', None)
 config.bind('Fd', 'hint all download')
 config.bind('Ff', 'hint all tab')
@@ -104,19 +120,19 @@ config.bind('J', 'run-with-count 5 scroll down')
 config.bind('K', 'run-with-count 5 scroll up')
 config.bind('O', 'set-cmd-text -s :open')
 # config.bind('Sb', None)
-config.bind('Sh', 'open -t qute://history')
-config.bind('Sq', 'open -t qute://bookmarks')
-config.bind('Ss', 'open -t qute://settings')
-config.bind('St', 'config-cycle tabs.show always never')
+config.bind('sh', 'open -t qute://history')
+config.bind('sq', 'open -t qute://bookmarks')
+config.bind('ss', 'open -t qute://settings')
+config.bind('st', 'config-cycle tabs.show always never')
+config.bind('sc', 'spawn gvim .config/qutebrowser/config.py')
+config.bind('sC', 'config-source ;; message-info "READY!"')
 config.bind('T', 'set-cmd-text -s :open -t')
 config.bind('V', 'spawn --userscript view_in_mpv')
-# config.bind('b', None)
-config.bind('bc', 'spawn gvim .config/qutebrowser/config.py')
-config.bind('bC', 'config-source ;; message-info "READY!"')
-config.bind('bJ', 'set --pattern *://{url:host}/* content.javascript.enabled false ;; reload ;; message-info "JS OFF"')
-config.bind('bj', 'set --pattern *://{url:host}/* content.javascript.enabled true ;; reload ;; message-info "JS ON"')
-config.bind('bs', 'set --pattern *://{url:host}/* content.ssl_strict false ;; reload ;; message-info "ACCEPT SSL CERT"')
-config.bind('bS', 'set --pattern *://{url:host}/* content.ssl_strict true ;; reload ;; message-info "FORGET SSL CERT"')
+config.unbind('B')
+config.bind('sJ', 'set --pattern *://{url:host}/* content.javascript.enabled false ;; reload ;; message-info "JS OFF"')
+config.bind('sj', 'set --pattern *://{url:host}/* content.javascript.enabled true ;; reload ;; message-info "JS ON"')
+config.bind('sk', 'set --pattern *://{url:host}/* content.ssl_strict false ;; reload ;; message-info "ACCEPT SSL CERT"')
+config.bind('sK', 'set --pattern *://{url:host}/* content.ssl_strict true ;; reload ;; message-info "FORGET SSL CERT"')
 config.bind('f', 'hint')
 config.bind('g#', 'tab-focus last')
 config.bind('gg', 'scroll top')
