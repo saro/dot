2a2a4de1ebbf125e720f40288990a6bf0c382721
Rosario Contarino <contarino.rosario@gmail.com>
2021-04-27 18:42:04 +0200

    Fix floating window

--- .i3/float_config.sh
+++ .i3/float_config.sh
@@ -15,7 +15,7 @@ bindcode 133 [instance="ttttt"] scratchpad show
 for_window [instance="ttttt"] move position 0 24
 for_window [instance="ttttt"] resize set ${DISPLAY_W} $((${DISPLAY_H} * 60 / 100))
 for_window [instance="ttttt"] scratchpad show
-exec ${TERMINAL} -name 'ttttt' -e \"/bin/bash -c 'source ~/shell/tmux && tx'\"
+exec ${TERMINAL} -name 'ttttt' -e /bin/bash -c 'source ~/shell/tmux && tx'
 
 # Add Pulsemixer floating window (width = 100%; height 40%; pos_x = 0%; pos_y = 50%)
 # for_window [instance="pulsemixer"] move to scratchpad
